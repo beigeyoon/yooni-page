2072f2f4669bd5c28a35e2cc26eeed616a40b495
beigeyoon <beige.yoon@gmail.com>
2025-02-04 17:05:00 +0900

    refac. NavBar refactoring

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -8,61 +8,25 @@ import {
 } from '@/components/ui/navigation-menu';
 import { buttonVariants } from '../ui/button';
 import AuthStatus from './AuthStatus';
+import { NAV_MENUS } from '@/constants';
 
 export function NavBar() {
   return (
     <NavigationMenu className="flex max-w-none items-center justify-between border-b border-neutral-400 p-4">
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link
-            href="/about"
-            legacyBehavior
-            passHref>
-            <NavigationMenuLink className={buttonVariants({ variant: 'link' })}>
-              About
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link
-            href="/projects"
-            legacyBehavior
-            passHref>
-            <NavigationMenuLink className={buttonVariants({ variant: 'link' })}>
-              Projects
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link
-            href="/travels"
-            legacyBehavior
-            passHref>
-            <NavigationMenuLink className={buttonVariants({ variant: 'link' })}>
-              Travels
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link
-            href="/devs"
-            legacyBehavior
-            passHref>
-            <NavigationMenuLink className={buttonVariants({ variant: 'link' })}>
-              Devs
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link
-            href="/photos"
-            legacyBehavior
-            passHref>
-            <NavigationMenuLink className={buttonVariants({ variant: 'link' })}>
-              Photos
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        {NAV_MENUS.map((menu, idx) => (
+          <NavigationMenuItem key={idx}>
+            <Link
+              href={menu.path}
+              legacyBehavior
+              passHref>
+              <NavigationMenuLink
+                className={buttonVariants({ variant: 'link' })}>
+                {menu.title}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
       </NavigationMenuList>
       <AuthStatus />
     </NavigationMenu>
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const NAV_MENUS = [
+  { title: 'About', path: '/about' },
+  { title: 'Projects', path: '/projects' },
+  { title: 'Travels', path: '/travels' },
+  { title: 'Devs', path: '/devs' },
+  { title: 'Photos', path: '/photos' },
+]
