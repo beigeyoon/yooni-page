225ce84d918ab0c6e206d36bf0510d0c962825c2
beigeyoon <beige.yoon@gmail.com>
2025-06-24 23:19:49 +0900

    feat. imageUpload를 서버단에서 처리하도록 변경

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -1,25 +1,20 @@
-import { supabase } from "@/lib/supabase";
-
 async function uploadImage(file: File) {
-  const fileName = `${Date.now()}_${file.name}`;
-  const { error } = await supabase.storage
-    .from('images')
-    .upload(fileName, file, {
-      cacheControl: '3600',
-      upsert: false,
-      contentType: file.type,
-    });
+  const formData = new FormData();
+  formData.append('file', file);
 
-  if (error) {
-    console.error('❌ 이미지 업로드 실패: ', error.message);
+  const res = await fetch('/api/upload', {
+    method: 'POST',
+    body: formData,
+  });
+
+  const result = await res.json();
+
+  if (!res.ok) {
+    console.error('❌ 이미지 업로드 실패:', result.error);
     return null;
   }
 
-  const { data: publicUrlData } = supabase.storage
-    .from('images')
-    .getPublicUrl(fileName);
-
-  return publicUrlData?.publicUrl || null;
+  return result.url;
 };
 
 export default uploadImage;
